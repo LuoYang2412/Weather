3fd5d513d5c7e9128c1f6e355fff42dc56fe842b
Luayang <toluoyang@qq.com>
2018-09-27 18:23:39 +0800

    修改ignore，添加Google线程池类

--- app/src/main/java/com/luoyang/weather/rsop/CityRsop.java
+++ app/src/main/java/com/luoyang/weather/rsop/CityRsop.java
@@ -2,10 +2,11 @@ package com.luoyang.weather.rsop;
 
 import android.support.annotation.Keep;
 
-import com.google.gson.Gson;
+import com.luoyang.weather.AppExecutors;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Keep
 public class CityRsop {
@@ -21,7 +22,25 @@ public class CityRsop {
     }
 
     public String getData() {
-        
+        new AppExecutors().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    FileReader fileReader = new FileReader("file:///android_asset/city.json");
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String tempString = "";
+                    while ((tempString = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(tempString);
+                    }
+                    bufferedReader.close();
+                    String cityDataString = stringBuilder.toString();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         return "";
     }
 }
